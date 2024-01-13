8f4794ff9336ef0eae4f7657e9fc19edad9ba004
jennananas <100166354+jennananas@users.noreply.github.com>
2024-01-13 12:49:48 +0100

    correct colors

--- src/components/EngagementCard.tsx
+++ src/components/EngagementCard.tsx
@@ -8,7 +8,7 @@ export default function EngagementCard({ engagement }: EngagementProps) {
     return (
         <div className="flex-shrink-0 grid grid-rows-[4fr, 1fr, 1fr] gap-10 mr-28">
             <img src={engagement.thumb} alt="logo" className="h-10 object-contain" />
-            <h3 className="uppercase font-bold relative text-lg before:content-[''] before:absolute before:top-[-25px] before:left-0 before:w-5 before:h-[2px] before:bg-black">{engagement.name}</h3>
+            <h3 className="uppercase font-bold relative text-lg before:content-[''] before:absolute before:top-[-25px] before:left-0 before:w-5 before:h-[2px] before:bg-black-950">{engagement.name}</h3>
             <p>{engagement.description}</p>
         </div>
     )
